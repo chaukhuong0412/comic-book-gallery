35b95643321f385f3fe45a955aede85a6a089d7b
Dinh Khuong Chau <chaukhuong0412@gmail.com>
2018-07-20 21:20:07 +0700

    Update the comic book model

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { set; get; }
+        public string SeriesTitle { set; get; }
+        public int IssueNumber { set; get; }
+        public string Description { set; get; }
+        public Artist[] Artists { set; get; }
+        public bool Favorite { set; get; }
+
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
+        public string CoverImageFileName => SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+    }
+}
